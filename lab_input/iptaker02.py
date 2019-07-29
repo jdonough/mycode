fe6ba38327b84983803f3b5112f632ec13cf80ae
Jay Donough <jay.donough@gmail.com>
2019-07-29 13:52:58 -0400

    backup

---
+++ lab_input/iptaker02.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+user_input = input("Please enter an IPv4 IP address:")
+print("You told me the IPv4 address is:" + user_input)
+print("You told me the IPv4 address is:", user_input)
+user_input1 = input ("Please enter vendor name:")
+print("You told me the vendor is:", user_input1)
