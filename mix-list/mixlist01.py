ce52b580fb45ad4e707373ab0312ba2c842bdd7c
Jay Donough <jay.donough@gmail.com>
2019-07-29 15:24:01 -0400

    after lab

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -8,4 +8,4 @@ print("The second item in the list (port): " + str(my_list[1]) )
 print("The last item in the list (state): " + my_list[2])
 # Second list
 new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
-print("When I "+ new_list[5], "into IP address "+ new_list[3], "or " + new_list[4], "I am unable to ping ports " + str(new_list[0]) + ", " + str(new_list[1]), "or " + str(new_list[2]))
+print("When I "+ new_list[5], "into IP address "+ new_list[3], "or " + new_list[4], "I am unable to ping ports " + str(new_list[0]) + ", " + str(new_list[1]), "or " + str(new_list[2]) + ".")
