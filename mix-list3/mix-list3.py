1c56ab93577b7548dc9aac465929d2cafeb67fb1
Jay Donough <jay.donough@gmail.com>
2019-07-29 15:59:56 -0400

    after list lab

---
+++ mix-list3/mix-list3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+list1 = ['Cisco_nxos', 'arista_eos', 'cisco_ios']
+print(list1)
+list1.extend(['Juniper'])
+print(list1)
+list1.append(['10.1.1.1', '10.2.0.1', '10.3.0.1'])
+print(list1)
+print(list1[4])
+print(list1[4][0])
