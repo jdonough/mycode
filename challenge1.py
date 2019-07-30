7569a1f76a86a60253b03b83f53f82be737a7bc5
Jay Donough <jay.donough@gmail.com>
2019-07-30 09:18:15 -0400

    post dict lab

---
+++ challenge1.py
@@ -0,0 +1,3 @@
+#! /usr/bin/env python3
+list = [ 3, "Nina", "pinta", "Santa-Maria"]
+print("Columbus had " + str(list[0]) + " ships: the " + list[1] + ", the " + list[2] + ", and the " + list[3] ".")
