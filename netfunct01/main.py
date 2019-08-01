5af49a8a5bee6fee1c231b091ecaed9a8895e08f
Jay Donough <jay.donough@gmail.com>
2019-08-01 11:40:13 -0400

    ifaces

--- netfunct01/main.py
+++ netfunct01/main.py
@@ -1,15 +1,14 @@
 #! /usr/bin/env python3
-
+'''function lab'''
 def commandpush(devicecmd):
+    '''key function'''
     for coffeetime in devicecmd.keys():
-        print('Handshaking. .. ... connecting with \n' + coffeetime )
+        print('Handshaking. .. ... connecting with \n' + coffeetime)
         for mycmds in devicecmd[coffeetime]:
             print('Attempting to sending command --> \n ' + mycmds)
-
 def main():
+    '''main function'''
     work2do = {"10.1.0.1":["interface eth1/2", "no shut"], "10.2.0.1":["interface eth1/1", "shutdown"]}
     print("Welcome to the network device command pusher\n")
     commandpush(work2do)
-
 main()
-    
---
+++ smallscript.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+''' print string '''
+
+def main():                      # define funct
+    '''main funct '''
+    mynote = 'print this string' # define string
+    print(mynote)                # displat string
+
+main()
